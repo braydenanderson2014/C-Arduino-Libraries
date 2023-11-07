ae615ba3f81abdea05ec85a52acbc905958f1913
Brayden Anderson <braydenanderson2014@gmail.com>
2023-11-06 22:31:38 -0700

    Update Properties.h

--- lib/Properties/src/Properties.h
+++ lib/Properties/src/Properties.h
@@ -36,20 +36,39 @@ public:
             return *this;
         }
 
+        // Postfix increment
+        PropertiesIterator operator++(int) {
+            PropertiesIterator tmp(*this);
+            operator++(); // prefix-increment this instance
+            return tmp;   // return value before increment
+        }
+
         bool operator!=(const PropertiesIterator& other) const {
             return it != other.it;
         }
 
         // Assuming `Hashtable::Iterator` properly dereferences to an `Entry`
-        Hashtable<String, String>::KeyValuePair operator*() const {
+            Hashtable<String, String>::KeyValuePair operator*() const {
+                return *it; // This assumes that `*it` dereferences to a KeyValuePair
+            }
+        /*
+        // Dereference operator to return a key-value pair
+            
+
+             Hashtable<String, String>::KeyValuePair operator*() const {
             auto entry = *it; // This assumes that `*it` gives you an `Entry` or something similar.
             return {entry->key, entry->value}; // Construct and return a KeyValuePair.
         }
 
-    
+        */
         // To make it easier to use without std::pair, you could provide methods to get the key and value:
-        String key() const { return (*it)->key; }
-        String value() const { return (*it)->value; }
+        String key() const { 
+            return (*it).key; 
+        }
+
+        String value() const { 
+            return (*it).value; 
+        }
     };
 
     PropertiesIterator begin() {
