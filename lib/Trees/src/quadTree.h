e48f1819fcadd9b31ecf84336bea274aa67693e5
Brayden Anderson <braydenanderson2014@gmail.com>
2024-02-27 15:32:34 -0700

    More Tree Implementation Repairs

--- lib/Trees/src/quadTree.h
+++ lib/Trees/src/quadTree.h
@@ -1,10 +1,171 @@
 #ifndef QUAD_TREE_h
 #define QUAD_TREE_h
+struct Point {
+    int x;
+    int y;
+};
 
+struct QuadNode {
+    Point point;
+    QuadNode* NW;
+    QuadNode* NE;
+    QuadNode* SW;
+    QuadNode* SE;
+
+    bool isLeaf() {
+        return NW == nullptr && NE == nullptr && SW == nullptr && SE == nullptr;
+    }
+};
 class QuadTree {
     public:
-        QuadTree();
-        ~QuadTree();
+    QuadTree() : root(nullptr) {}
+    
+    ~QuadTree(){
+        clear(root);
+    }
+
+    void insert(QuadNode* node, Point point) {
+    // Determine in which quadrant the point belongs and insert it there
+        if (point.x < node->point.x) { // west
+            if (point.y < node->point.y) { // south
+                insert(node->SW, point);
+            } else { // north
+                insert(node->NW, point);
+            }
+        } else { // east
+            if (point.y < node->point.y) { // south
+                insert(node->SE, point);
+            } else { // north
+                insert(node->NE, point);
+            }
+        }
+    }
+
+    void split(QuadNode* node) {
+        // Create four child nodes
+        node->NW = new QuadNode();
+        node->NE = new QuadNode();
+        node->SW = new QuadNode();
+        node->SE = new QuadNode();
+
+        // Reassign the point in the original node to the appropriate child node
+        insert(node, node->point);
+        node->point = Point(); // Reset the point in the original node
+    }
+
+    bool search(Point point) {
+        return search(root, point);
+    }
+
+    void clear(QuadNode* node){
+        if(node == nullptr){
+            return;
+        }
+        clear(node->NW);
+        clear(node->NE);
+        clear(node->SW);
+        clear(node->SE);
+        delete node;
+    }
+
+    void remove(Point point) {
+        root = remove(root, point);
+    
+    }
+
+    Point findMin(QuadNode* node){
+        if(node == nullptr){
+            return Point();
+        }
+        if(node->NW == nullptr){
+            return node->point;
+        }
+        return findMin(node->NW);
+    }
+
+    private:
+    QuadNode* root;
+
+    bool isLeaf() {
+        return isLeaf(root);
+    }
+
+    bool isLeaf(QuadNode* node) {
+        return node->NW == nullptr && node->NE == nullptr && node->SW == nullptr && node->SE == nullptr;
+    }
+
+    void insert(Point point) {
+        if (root == nullptr) {
+            root = new QuadNode{point, nullptr, nullptr, nullptr, nullptr};
+        } else {
+            insert(root, point);
+        }
+    }
+
+    bool search(QuadNode* node, Point point) {
+        if (node == nullptr) {
+            return false;
+        }
+
+        if (node->point.x == point.x && node->point.y == point.y) {
+            return true;
+        }
+
+        if (point.x < node->point.x) { // west
+            if (point.y < node->point.y) { // south
+                return search(node->SW, point);
+            } else { // north
+                return search(node->NW, point);
+            }
+        } else { // east
+            if (point.y < node->point.y) { // south
+                return search(node->SE, point);
+            } else { // north
+                return search(node->NE, point);
+            }
+        }
+    }
+
+    QuadNode* remove(QuadNode* node, Point point){
+        if(node == nullptr){
+            return nullptr;
+        }
+
+        if(node->point.x == point.x && node->point.y == point.y){
+            if(node->NW == nullptr && node->NE == nullptr && node->SW == nullptr && node->SE == nullptr){
+                delete node;
+                return nullptr;
+            }
+            if(node->NW == nullptr){
+                if(node->NE == nullptr){
+                    if(node->SW == nullptr){
+                        return node->SE;
+                    }
+                    if(node->SE == nullptr){
+                        return node->SW;
+                    }
+                }
+                if(node->SW == nullptr){
+                    if(node->SE == nullptr){
+                        return node->NE;
+                    }
+                }
+            }
+            if(node->NE == nullptr){
+                if(node->SW == nullptr){
+                    if(node->SE == nullptr){
+                        return node->NW;
+                    }
+                }
+            }
+            Point temp = findMin(node->NE);
+            node->point = temp;
+            node->NE = remove(node->NE, temp);
+            return node;
+        }
+    }
+
+    
 };
 
 
--- lib/Trees/src/redblackTree.h
+++ lib/Trees/src/redblackTree.h
@@ -3,407 +3,104 @@
 
 #include <Arduino.h>
 
+
+enum Color {RED, BLACK};
+
+template <typename T>
+struct RedBlackNode {
+    T data;
+    bool color;
+    RedBlackNode *left, *right, *parent;
+
+    // Constructor
+    RedBlackNode(T data) {
+       this->data = data;
+       left = right = parent = NULL;
+       this->color = RED;
+    }
+};
+
+
 template <typename T>
 class RedBlackTree{
     private:
-        struct RedBlackNode{
-            T data;
-            RedBlackNode *left;
-            RedBlackNode *right;
-            RedBlackNode *parent;
-            bool isBlack;
-        };
-        RedBlackNode *root;
+        RedBlackNode<T> *root;
 
-        RedBlackNode *insert(RedBlackNode *node, T data){
-            if(node == NULL){
-                RedBlackNode *newNode = new RedBlackNode;
-                newNode->data = data;
-                newNode->left = NULL;
-                newNode->right = NULL;
-                newNode->parent = NULL;
-                newNode->isBlack = false;
+        RedBlackNode<T>* BSTInsert(RedBlackNode<T>* root, RedBlackNode<T>* newNode){
+            if(root == NULL){
                 return newNode;
             }
-            if(data < node->data){
-                node->left = insert(node->left, data);
-                node->left->parent = node;
-            } else if(data > node->data){
-                node->right = insert(node->right, data);
-                node->right->parent = node;
+            if(newNode->data < root->data){
+                root->left = BSTInsert(root->left, newNode);
+                root->left->parent = root;
+            }else if(newNode->data > root->data){
+                root->right = BSTInsert(root->right, newNode);
+                root->right->parent = root;
             }
-            return node;
+            return root;
         }
 
-        RedBlackNode *remove(RedBlackNode *node, T data){
-            if(node == NULL){
-                return node;
-            }
-            if(data < node->data){
-                node->left = remove(node->left, data);
-            } else if(data > node->data){
-                node->right = remove(node->right, data);
-            } else {
-                if(node->left == NULL || node->right == NULL){
-                    RedBlackNode *temp = node->left ? node->left : node->right;
-                    if(temp == NULL){
-                        temp = node;
-                        node = NULL;
-                    } else {
-                        *node = *temp;
-                    }
-                    delete temp;
-                } else {
-                    RedBlackNode *temp = findMin(node->right);
-                    node->data = temp->data;
-                    node->right = remove(node->right, temp->data);
-                }
-            }
-            return node;
-        }
+        void fixViolation(RedBlackNode<T>* &root, RedBlackNode<T>* &newNode){
+            RedBlackNode<T> *parent = NULL;
+            RedBlackNode<T> *grandparent = NULL;
 
-        RedBlackNode *findMin(RedBlackNode *node){
-            while(node->left != NULL){
-                node = node->left;
-            }
-            return node;
-        }
+            while((newNode != root) && (newNode->color != BLACK) && (newNode->parent->color == RED)){
+                parent = newNode->parent;
+                grandparent = newNode->parent->parent;
 
-        RedBlackNode *rotateLeft(RedBlackNode *node){
-            RedBlackNode *temp = node->right;
-            node->right = temp->left;
-            if(temp->left != NULL){
-                temp->left->parent = node;
-            }
-            temp->parent = node->parent;
-            if(node->parent == NULL){
-                root = temp;
-            } else if(node == node->parent->left){
-                node->parent->left = temp;
-            } else {
-                node->parent->right = temp;
-            }
-            temp->left = node;
-            node->parent = temp;
-            return temp;
-        }
-
-        RedBlackNode *rotateRight(RedBlackNode *node){
-            RedBlackNode *temp = node->left;
-            node->left = temp->right;
-            if(temp->right != NULL){
-                temp->right->parent = node;
-            }
-            temp->parent = node->parent;
-            if(node->parent == NULL){
-                root = temp;
-            } else if(node == node->parent->right){
-                node->parent->right = temp;
-            } else {
-                node->parent->left = temp;
-            }
-            temp->right = node;
-            node->parent = temp;
-            return temp;
-        }
-
-        void fixInsert(RedBlackNode *node){
-            RedBlackNode *parent = NULL;
-            RedBlackNode *grandparent = NULL;
-            while((node != root) && (node->isBlack != false) && (node->parent->isBlack == false)){
-                parent = node->parent;
-                grandparent = node->parent->parent;
                 if(parent == grandparent->left){
-                    RedBlackNode *uncle = grandparent->right;
-                    if(uncle != NULL && uncle->isBlack == false){
-                        grandparent->isBlack = false;
-                        parent->isBlack = true;
-                        uncle->isBlack = true;
-                        node = grandparent;
-                    } else {
-                        if(node == parent->right){
-                            node = parent;
-                            rotateLeft(node);
+                    RedBlackNode<T> *uncle = grandparent->right;
+                    if(uncle != NULL && uncle->color == RED){
+                        grandparent->color = RED;
+                        parent->color = BLACK;
+                        uncle->color = BLACK;
+                        newNode = grandparent;
+                    }else{
+                        if(newNode == parent->right){
+                            leftRotate(root, parent);
+                            newNode = parent;
+                            parent = newNode->parent;
                         }
-                        rotateRight(grandparent);
-                        bool temp = parent->isBlack;
-                        parent->isBlack = grandparent->isBlack;
-                        grandparent->isBlack = temp;
-                        node = parent;
+                        rightRotate(root, grandparent);
+                        swap(parent->color, grandparent->color);
+                        newNode = parent;
                     }
-                } else {
-                    RedBlackNode *uncle = grandparent->left;
-                    if(uncle != NULL && uncle->isBlack == false){
-                        grandparent->isBlack = false;
-                        parent->isBlack = true;
-                        uncle->isBlack = true;
-                        node = grandparent;
-                    } else {
-                        if(node == parent->left){
-                            node = parent;
-                            rotateRight(node);
+                }else{
+                    RedBlackNode<T> *uncle = grandparent->left;
+                    if((uncle != NULL) && (uncle->color == RED)){
+                        grandparent->color = RED;
+                        parent->color = BLACK;
+                        uncle->color = BLACK;
+                        newNode = grandparent;
+                    }else{
+                        if(newNode == parent->left){
+                            rightRotate(root, parent);
+                            newNode = parent;
+                            parent = newNode->parent;
                         }
-                        rotateLeft(grandparent);
-                        bool temp = parent->isBlack;
-                        parent->isBlack = grandparent->isBlack;
-                        grandparent->isBlack = temp;
-                        node = parent;
+                        leftRotate(root, grandparent);
+                        swap(parent->color, grandparent->color);
+                        newNode = parent;
                     }
                 }
             }
-            root->isBlack = true;
+            root->color = BLACK;
         }
-
-        void fixRemove(RedBlackNode *node){
-            if(node == NULL){
-                return;
-            }
-            if(node == root){
-                root = NULL;
-                return;
-            }
-            if(node->isBlack == false || (node->left != NULL && node->left->isBlack == false) || (node->right != NULL && node->right->isBlack == false)){
-                RedBlackNode *temp = node->left != NULL ? node->left : node->right;
-                if(node == node->parent->left){
-                    node->parent->left = temp;
-                    if(temp != NULL){
-                        temp->parent = node->parent;
-                    }
-                    temp->isBlack = true;
-                } else {
-                    node->parent->right = temp;
-                    if(temp != NULL){
-                        temp->parent = node->parent;
-                    }
-                    temp->isBlack = true;
-                }
-                delete node;
-            } else {
-                RedBlackNode *sibling = NULL;
-                RedBlackNode *parent = NULL;
-                RedBlackNode *temp = node;
-                temp->isBlack = true;
-                while(temp != root && temp->isBlack == true){
-                    parent = temp->parent;
-                    if(temp == parent->left){
-                        sibling = parent->right;
-                        if(sibling->isBlack == false){
-                            sibling->isBlack = true;
-                            parent->isBlack = false;
-                            rotateLeft(parent);
-                            sibling = parent->right;
-                        }
-                        if((sibling->left == NULL || sibling->left->isBlack == true) && (sibling->right == NULL || sibling->right->isBlack == true)){
-                            sibling->isBlack = false;
-                            temp = parent;
-                        } else {
-                            if(sibling->right == NULL || sibling->right->isBlack == true){
-                                sibling->left->isBlack = true;
-                                sibling->isBlack = false;
-                                rotateRight(sibling);
-                                sibling = parent->right;
-                            }
-                            sibling->isBlack = parent->isBlack;
-                            parent->isBlack = true;
-                            sibling->right->isBlack = true;
-                            rotateLeft(parent);
-                            temp = root;
-                        }
-                    } else {
-                        sibling = parent->left;
-                        if(sibling->isBlack == false){
-                            sibling->isBlack = true;
-                            parent->isBlack = false;
-                            rotateRight(parent);
-                            sibling = parent->left;
-
-                        }
-                        if((sibling->left == NULL || sibling->left->isBlack == true) && (sibling->right == NULL || sibling->right->isBlack == true)){
-                            sibling->isBlack = false;
-                            temp = parent;
-                        } else {
-                            if(sibling->left == NULL || sibling->left->isBlack == true){
-                                sibling->right->isBlack = true;
-                                sibling->isBlack = false;
-                                rotateLeft(sibling);
-                                sibling = parent->left;
-                            }
-                            sibling->isBlack = parent->isBlack;
-                            parent->isBlack = true;
-                            sibling->left->isBlack = true;
-                            rotateRight(parent);
-                            temp = root;
-                        }
-                    }
-                }
-                if(temp != NULL){
-                    temp->isBlack = true;
-                }
-            }
-        }
-
-        void clear(RedBlackNode *node){
-            if(node == NULL){
-                return;
-            }
-            clear(node->left);
-            clear(node->right);
-            delete node;
-        }
-
-
     public:
-        RedBlackTree(){
-            root = NULL;
-        }
-
+        RedBlackTree() {root = NULL;}
         ~RedBlackTree(){
-            delete root;
-        }
-
-        void insert(T data){
-            root = insert(root, data);
-            fixInsert(root);
-        }
-
-        void remove(T data){
-            root = remove(root, data);
-            fixRemove(root);
-        }
-
-        void inOrder(){
-            inOrder(root);
-        }
-
-        void preOrder(){
-            preOrder(root);
-        }
-
-        void postOrder(){
-            postOrder(root);
-        }
-
-        void inOrder(RedBlackNode *node){
-            if(node == NULL){
-                return;
-            }
-            inOrder(node->left);
-            Serial.println(node->data);
-            inOrder(node->right);
-        }
-
-        void preOrder(RedBlackNode *node){
-            if(node == NULL){
-                return;
-            }
-            Serial.println(node->data);
-            preOrder(node->left);
-            preOrder(node->right);
-        }
-
-        void postOrder(RedBlackNode *node){
-            if(node == NULL){
-                return;
-            }
-            postOrder(node->left);
-            postOrder(node->right);
-            Serial.println(node->data);
-        }
-
-        int findMin(){
-            RedBlackNode *temp = findMin(root);
-            return temp->data;
-        }
-
-        int findMax(){
-            RedBlackNode *temp = root;
-            while(temp->right != NULL){
-                temp = temp->right;
-            }
-            return temp->data;
-        }
-
-        int findHeight(){
-            return findHeight(root);
-        }
-
-        int findHeight(RedBlackNode *node){
-            if(node == NULL){
-                return -1;
-            }
-            int left = findHeight(node->left);
-            int right = findHeight(node->right);
-            if(left > right){
-                return left + 1;
-            } else {
-                return right + 1;
-            }
-        }
-
-        bool find(T data){
-            return find(root, data);
-        }
-
-        bool find(RedBlackNode *node, T data){
-            if(node == NULL){
-                return false;
-            }
-            if(data < node->data){
-                return find(node->left, data);
-            } else if(data > node->data){
-                return find(node->right, data);
-            } else {
-                return true;
-            }
-        }
-
-        void print(){
-            print(root, 0);
-        }
-
-        void print(RedBlackNode *node, int space){
-            if(node == NULL){
-                return;
-            }
-            space += 10;
-            print(node->right, space);
-            Serial.println();
-            for(int i = 10; i < space; i++){
-                Serial.print(" ");
-            }
-            Serial.println(node->data);
-            print(node->left, space);
-        }
-
-        void clear(){
             clear(root);
         }
 
-        void clear(RedBlackNode *node){
-            if(node == NULL){
-                return;
-            }
-            clear(node->left);
-            clear(node->right);
-            delete node;
+        void insert(const T &data){
+            RedBlackNode<T> *newNode = new RedBlackNode<T>(data);
+            root = BSTInsert(root, newNode);
+            fixViolation(root, newNode);
         }
+        
+        
 
-        void clearTree(){
-            clear(root);
-            root = NULL;
-        }
-
-        void clearTree(RedBlackNode *node){
-            if(node == NULL){
-                return;
-            }
-            clearTree(node->left);
-            clearTree(node->right);
-            delete node;
-        }
-
-
-
+    
 };
 
 #endif //   REDBLACK_TREE_h
