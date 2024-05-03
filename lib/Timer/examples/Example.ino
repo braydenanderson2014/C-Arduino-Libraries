c51bf7bff636dbb7bbedad680f81345555e974e0
Brayden Anderson <braydenanderson2014@gmail.com>
2024-05-03 10:55:08 -0600

    Update to Timer & Repository
    Fixed The Timer Library. All known issues have been resolved.

--- lib/Timer/examples/Example.ino
+++ lib/Timer/examples/Example.ino
@@ -1,56 +1,104 @@
-#include <SimpleArduinoTimer.h>
+#include <Arduino.h>
+// #include <CustomString.h>
+// #include <MemoryManager.h>
+// #include <MathLib.h>
+// #include <Numeric_Limits.h>
+// #include <LittleFSProperties.h>
+#include "SimpleArduinoTimer.h"
 
-Timer myTimer;  // Create an instance of the Timer class
+Timer timer = new Timer(false);
+// SDList <String> strings(10, "pagefile.dat");
+// ArrayList <String> list = ArrayList<String>(ArrayList<String>::DYNAMIC, 10);
+void setup()
+{
+    /*
+    Custom_String::String str = "Hello, World!"; // Create a string object with the value "Hello, World!"
+    Serial.begin(9600); // Start the serial communication
+    Serial.println(str.C_STR()); // Hello, World!
 
-void setup() {
+    str.append(" This is a test!"); // Append the string " This is a test!" to the end of the string
+    Serial.println(str.C_STR()); // Hello, World! This is a test!
+
+    Serial.println(str.Capacity()); // Strings capacity
+
+    int i = 0;
+    Serial.println(str.indexOf('o', i) + i); // 4
+
+    Custom_String::String str2 = "9999";
+    Serial.println(Random(str.toInt(), 5));
+
+    float f = 3.14159*str.toFloat(); // Converts String to float
+    Serial.println(f); // 9.42477
+
+    Serial.println(Round(f)*str.toInt()*str.toFloat()/2.25); // 18
+
+    */
     Serial.begin(9600);
-    myTimer.setTargetMinutes(5);  // Set the target duration to 5 minutes
-    myTimer.start();  // Start the timer
+    while (!Serial)
+        ;
+    timer.setTargetMinutes(5); // Set the target duration to 5 minutes
+    unsigned long x = 1000;
+
+    
+    timer.start(); // Start the timer
+    while(!timer.hasReachedTarget()){
+        if(timer.checkTimer(x)){ // checks if a second has passed (This is a non blocking way of only printing the timer every second.)
+            Serial.println(String(timer.remainingTime())); //1 side effect of remainingTime() is that it will auto adjust based off of the time remaining. all without you knowing.
+            x += 10000;
+        }
+    }
+    Serial.println("Timer has reached target duration!");
+    timer.reset(); // Reset the timer
+    x = 1000; //sets the variable x back to 1 second (THIS IS NOT NECESSARY, JUST FOR DEMONSTRATION PURPOSES)
+
+
+    
+    timer.start();
+    while (!timer.hasReachedTarget())
+    {
+        if (timer.checkTimer(x)) // checks if a second has passed (This is a non blocking way of only printing the timer every second.)
+        {
+            timer.printTimeRemaining(); // Prints the remaining time on the timer
+            x += 1000;
+        }
+    }
+
+    timer.setTargetSeconds(10); // Set the target duration to 10 seconds
+    timer.start(); // Start the timer
+    while (!timer.hasReachedTarget())
+    {
+        if (timer.checkTimer(x)) // checks if a second has passed (This is a non blocking way of only printing the timer every second.)
+        {
+            timer.printTimeRemaining(); // Prints the remaining time on the timer
+            x += 1000;
+        }
+    }
+    Serial.println("Timer has reached target duration!");
+
+    timer.setTargetHours(2); // Set the target duration to 1 hour
+    timer.start(); // Start the timer
+    while (!timer.hasReachedTarget())
+    {
+        if (timer.checkTimer(x)) // checks if a second has passed (This is a non blocking way of only printing the timer every second.)
+        {
+            timer.printTimeRemaining(); // Prints the remaining time on the timer
+            x += 1000;
+        }
+    }
+    Serial.println("Timer has reached target duration!");
+
+
+    //You can also call functions like 
+    //timer.pause(); // Pauses the timer
+    //timer.resume(); // Resumes the timer
+    //timer.stop(); // Stops the timer
+    //timer.clear(); // Clears the timer
+
+    
+
+
 }
 
-void loop() {
-    if (myTimer.checkTimer(5000)) {  // Check if 5 seconds have elapsed
-        Serial.println("5 seconds have elapsed!");
-        myTimer.reset();  // Reset the timer
-    }
-
-    if (myTimer.isTimerRunning()) {  // Check if the timer is running
-        Serial.println("Timer is running");
-    } else if (myTimer.isTimerPaused()) {  // Check if the timer is paused
-        Serial.println("Timer is paused");
-    } else {
-        Serial.println("Timer is stopped");
-    }
-
-    if (myTimer.hasReachedTarget()) {  // Check if the target duration has been reached
-        Serial.println("Target duration has been reached!");
-    }
-
-    unsigned long remaining = myTimer.remainingTime();  // Get the remaining time until the target duration is reached
-    Serial.print("Remaining time: ");
-    Serial.println(remaining);
-
-    DateTime now = myTimer.getRTCTime();  // Get the current time from the RTC
-    Serial.print("Current time: ");
-    Serial.print(now.year());
-    Serial.print("-");
-    Serial.print(now.month());
-    Serial.print("-");
-    Serial.print(now.day());
-    Serial.print(" ");
-    Serial.print(now.hour());
-    Serial.print(":");
-    Serial.print(now.minute());
-    Serial.print(":");
-    Serial.println(now.second());
-
-    myTimer.setTargetMinutes(1);  // Set the target duration to 1 minute
-    myTimer.pause();  // Pause the timer
-    delay(5000);  // Wait for 5 seconds
-    myTimer.resume();  // Resume the timer
-    delay(60000);  // Wait for 1 minute
-    myTimer.stop();  // Stop the timer
-    myTimer.syncWithRTC();  // Sync the elapsed time with the RTC
-    myTimer.setRTCTime(2022, 1, 1, 0, 0, 0);  // Set the RTC time to January 1, 2022 at midnight
-    myTimer.clear();  // Clear the timer
+void loop()
+{
 }
--- lib/Timer/src/SimpleArduinoTimer.h
+++ lib/Timer/src/SimpleArduinoTimer.h
@@ -15,9 +15,15 @@ private:
     bool useRTC;
     mutable RTC_DS3231 rtc;
     bool debug;
+    unsigned long remainingTimeOnTimer;
+    enum mode {Seconds, Minutes, Hours};
+    mode timerMode = Minutes;
+    String TimerName;
 public:
-    Timer(bool debug = false);
-
+    Timer(bool debug);
+    void setTimerName(String timerName);
+    String getTimerName() const;
+    void updateRemainingTime();
     void start();
     void stop();
     void reset();
@@ -26,7 +32,7 @@ public:
     void resume();
     void setUseRTC(bool useRTC);
     void begin();
-    
+    void printTimeRemaining();
     unsigned long elapsed();
     bool isTimerRunning() const;
     bool hasReachedTarget();
@@ -38,6 +44,7 @@ public:
     bool isTimerPaused() const;
     void setRTCTime(int year, int month, int day, int hour, int minute, int second);
     void setTargetDuration(unsigned long duration);
+    void setTargetSeconds(unsigned long seconds);
     void setTargetMinutes(unsigned long minutes);
     void setTargetHours(unsigned long hours);
     bool checkTimer(unsigned long duration);
